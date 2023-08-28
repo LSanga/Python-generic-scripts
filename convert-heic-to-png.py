691345e34011ced037c7eee881838eb59dc12a03
LSanga <51289040+LSanga@users.noreply.github.com>
2023-08-28 17:32:42 +0200

    Update convert-heic-to-png.py
    added:
    -choice between png and jpeg
    -status bar
    -default multithreading

--- convert-heic-to-png.py
+++ convert-heic-to-png.py
@@ -1,12 +1,14 @@
-#This script convert .heic images into png, using multithread as default (see line 37)
+#This script convert .heic images into png or jp(e)g, using multithread as default (see line 37)
+#JPEG and JPG are the same format but both input are accepted to avoid mistakes. In any case the resulting files will be saved as .jpg
 #To use it, copy the script in the folder where you have all the heic file and run with:
-#python3 convert-heic-to-png.py
+#python3 convert-heic-images.py
 
 
 from PIL import Image
 import pyheif
 import os
 import concurrent.futures
+from tqdm import tqdm
 
 def convert_heic_to_image(heic_path, destination_folder, image_format):
     try:
@@ -20,13 +22,18 @@ def convert_heic_to_image(heic_path, destination_folder, image_format):
             heif_file.stride,
         )
 
-        image_filename = os.path.splitext(os.path.basename(heic_path))[0] + f'.{image_format}'
-        image_path = os.path.join(destination_folder, image_filename)
-
-        image.save(image_path, format=image_format)
-        print(f"Converted {heic_path} to {image_path}")
+        image_filename = os.path.splitext(os.path.basename(heic_path))[0]
+        if image_format == 'png':
+            image_path = os.path.join(destination_folder, f"{image_filename}.png")
+            image.save(image_path, format='PNG')
+        elif image_format in ('jpeg', 'jpg'):  # Allow both 'jpeg' and 'jpg'
+            image_path = os.path.join(destination_folder, f"{image_filename}.jpg")
+            image.save(image_path, format='JPEG')
+            
+        return heic_path  # Return the path of the converted image
     except Exception as e:
         print(f"Error converting {heic_path}: {e}")
+        return None
 
 def main():
     source_folder = os.getcwd()  # Use the current working directory as the source folder
@@ -35,17 +42,22 @@ def main():
 
     heic_files = [filename for filename in os.listdir(source_folder) if filename.lower().endswith('.heic')]
 
-    image_format = input("Enter the desired image format (png or jpg): ").lower()
-    if image_format not in ('png', 'jpg'):
-        print("Invalid image format choice. Please choose 'png' or 'jpg'.")
+    image_format = input("Enter the desired image format (png, jpeg, or jpg): ").lower()
+    if image_format not in ('png', 'jpeg', 'jpg'):
+        print("Invalid image format choice. Please choose 'png', 'jpeg', or 'jpg'.")
         return
-		
-	#if you want to specify a fixed number of threads use the following:
+
+    # Create a ThreadPoolExecutor and use a list comprehension to submit tasks
+    #if you want to specify a fixed number of threads use the following:
     #with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        for heic_file in heic_files:
-            heic_path = os.path.join(source_folder, heic_file)
-            executor.submit(convert_heic_to_image, heic_path, destination_folder, image_format)
+        results = [executor.submit(convert_heic_to_image, os.path.join(source_folder, heic_file), destination_folder, image_format) for heic_file in heic_files]
+
+        # Create a progress bar using tqdm
+        with tqdm(total=len(results), desc="Converting") as pbar:
+            for future in concurrent.futures.as_completed(results):
+                if future.result() is not None:
+                    pbar.update(1)
 
 if __name__ == "__main__":
     main()
